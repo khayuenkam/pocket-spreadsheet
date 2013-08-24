ac5e367934f11470c1bc4fdbeea88958d3f11d59
khayuen <khayuen@yahoo.com>
2013-08-24 02:32:01 +0000

    Update the file paths name in server.js

--- server.js
+++ server.js
@@ -4,8 +4,8 @@
  */
 
 var express = require('express');
-var routes = require('./routes');
-var user = require('./routes/user');
+var routes = require('./app/controllers');
+var user = require('./app/controllers/user');
 var http = require('http');
 var path = require('path');
 
@@ -13,8 +13,9 @@ var app = express();
 
 // all environments
 app.set('port', process.env.PORT || 3000);
-app.set('views', __dirname + '/views');
+app.set('views', __dirname + '/app/views');
 app.set('view engine', 'ejs');
+app.use(express.compress());
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
@@ -22,20 +23,18 @@ app.use(express.methodOverride());
 app.use(express.cookieParser('your secret here'));
 app.use(express.session());
 app.use(app.router);
-app.use(require('stylus').middleware(__dirname + '/public'));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(require('stylus').middleware(__dirname + '/app/assets'));
+app.use(express.static(path.join(__dirname, '/app/assets')));
 
 // development only
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-
-
-
 app.get('/', routes.index);
 app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
+  console.log(__dirname);
   console.log('Express server listening on port ' + app.get('port'));
 });
