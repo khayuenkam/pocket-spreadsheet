2eae4abfbac68fd00411587c13e81c91902ed7dc
khayuen <khayuen@yahoo.com>
2013-08-24 02:59:07 +0000

    Remove console.log in server.js and add comma in package.json

--- server.js
+++ server.js
@@ -35,6 +35,5 @@ app.get('/', routes.index);
 app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
-  console.log(__dirname);
   console.log('Express server listening on port ' + app.get('port'));
 });
